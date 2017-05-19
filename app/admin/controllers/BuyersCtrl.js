ccb639b813c36bcceff43d77aa8b0a8bcc29cc4a
ericvadim <ericvadim@mail.com>
2017-05-19 11:42:43 +0200

    admin news page

--- app/admin/controllers/BuyersCtrl.js
+++ app/admin/controllers/BuyersCtrl.js
@@ -1,6 +1,74 @@
 'use strict';
 
-angular.module('app.admin').controller('BuyersController', function ($scope) {
+angular.module('app.admin').controller('BuyersController', function ($scope, $filter, $timeout, BuyersService) {
+    $scope.tableData = $scope.safeData = [];
+    $scope.currRow = {};
+    $scope.loading = true;
+    $scope.allCheck = false;
+    $scope.email = {
+        title: '',
+        content: ''
+    };
 
+    $scope.getData = function () {
+        $scope.loading = true;
 
+        $timeout(function () {
+            $scope.loading = false;
+            $scope.tableData = $scope.safeData = [
+                {id: 1, email: 'abc', purchased_date: '2017-05-19'},
+                {id: 2, email: 'abc', purchased_date: '2017-05-19'},
+                {id: 3, email: 'abc', purchased_date: '2017-05-19'},
+                {id: 4, email: 'abc', purchased_date: '2017-05-19'}
+            ];
+        }, 500);
+        return;
+
+        BuyersService.get().then(function (response) {
+            $scope.tableData = $scope.safeData = response.data;
+            $scope.loading = false;
+        });
+    };
+    $scope.getData();
+
+    $scope.deleteRow = function (rowId) {
+        if (confirm('Are you sure want to delete this?')) {
+            $scope.loading = true;
+            BuyersService.delete(rowId).then(function () {
+                $scope.getData();
+            });
+        }
+    };
+
+    $scope.checkAll = function () {
+        angular.forEach($scope.tableData, function (val) {
+            val.checked = $scope.allCheck;
+        })
+    };
+
+    $scope.getCheckedEmails = function () {
+        var emails = [];
+        angular.forEach($filter('filter')($scope.tableData, {checked: true}), function (val) {
+            emails[emails.length] = val.email;
+        });
+        return emails;
+    };
+
+    $scope.initMail = function () {
+        $scope.email.title = '';
+        $scope.email.content = '';
+    };
+
+    $scope.sendMail = function () {
+        $scope.loading = true;
+        var data = {
+            emails: $scope.getCheckedEmails(),
+            title: $scope.email.title,
+            content: $scope.email.content
+        };
+        BuyersService.sendMail(data).then(function () {
+            $('#myModal').modal('hide');
+            $scope.getData();
+        });
+    };
 });
---
+++ app/admin/services/BuyersService.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+
+    angular.module('app.admin')
+        .factory('BuyersService', ['$http', '$q', 'ServerURL', function ($http, $q, ServerURL) {
+            return {
+                get: function () {
+                    var url = ServerURL + 'news';
+                    var deferred = $q.defer();
+                    $http.get(url).then(function (res) {
+                        deferred.resolve(res);
+                    }, function (err) {
+                        deferred.reject(err);
+                    });
+                    return deferred.promise;
+                },
+                delete: function (rowId) {
+                    var deferred = $q.defer();
+                    var url = ServerURL + 'news?id=' + rowId;
+                    $http.delete(url).then(function (res) {
+                        deferred.resolve(res);
+                    }, function (err) {
+                        deferred.reject(err);
+                    });
+                    return deferred.promise;
+                },
+                sendMail: function (data) {
+                    var url = ServerURL + 'news';
+                    var deferred = $q.defer();
+                    $http({
+                        method: 'POST',
+                        url: url,
+                        headers: {'Content-Type': 'multipart/form-data'},
+                        data: data
+                    }).then(function (res) {
+                        deferred.resolve(res);
+                    }, function (err) {
+                        deferred.reject(err);
+                    });
+                    return deferred.promise;
+                }
+            };
+        }]);
+})();
