73315a28a242756489411d0f4c96a1a0caf9e91a
ericvadim <ericvadim@mail.com>
2017-05-20 08:22:40 +0200

    about page

--- app/home/controllers/AboutCtrl.js
+++ app/home/controllers/AboutCtrl.js
@@ -1,5 +1,18 @@
 "use strict";
 
 angular.module('app.home').controller('AboutController', function ($scope, $window) {
-
+    $scope.leftMenus = [
+        {id: 1, title: "High Concept", state: "high_concept"},
+        {id: 2, title: "Visual Style", state: "visual_style"},
+        {id: 3, title: "World", state: "world"},
+        {id: 4, title: "Game Play", state: "gameplay"},
+        {id: 5, title: "Character Classes", state: "character_classes"},
+        {id: 6, title: "Missions", state: "missions"},
+        {id: 7, title: "Tech", state: "tech"},
+        {id: 8, title: "Risks", state: "risks"},
+        {id: 9, title: "Extras", state: "extras"}
+    ];
+    $scope.scrollDown = function () {
+        $window.scrollTo(0, angular.element('.content-section').offset().top);
+    };
 })
--- app/home/module.js
+++ app/home/module.js
@@ -20,6 +20,80 @@ angular.module('app.home', ['ui.router']).config(function ($stateProvider) {
                 }
             }
         })
+        .state('about.high_concept', {
+            url: '/high_concept',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/high_concept.html'
+                }
+            }
+        })
+        .state('about.visual_style', {
+            url: '/visual_style',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/visual_style.html'
+                }
+            }
+        })
+        .state('about.world', {
+            url: '/world',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/world.html'
+                }
+            }
+        })
+        .state('about.gameplay', {
+            url: '/gameplay',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/gameplay.html'
+                }
+            }
+        })
+        .state('about.character_classes', {
+            url: '/character_classes',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/character_classes.html'
+                }
+            }
+        })
+        .state('about.missions', {
+            url: '/missions',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/missions.html'
+                }
+            }
+        })
+        .state('about.tech', {
+            url: '/tech',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/tech.html'
+                }
+            }
+        })
+        .state('about.risks', {
+            url: '/risks',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/risks.html'
+                }
+            }
+        })
+        .state('about.extras', {
+            url: '/extras',
+            views: {
+                "aboutContent": {
+                    templateUrl: 'app/home/views/about-templates/extras.html'
+                }
+            }
+        })
+
+
         .state('news', {
             url: '/news',
             views: {
