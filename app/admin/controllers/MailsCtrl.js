f9488d3173fb3a1f2fd3e305015d0685bd8320fe
ericvadim <ericvadim@mail.com>
2017-05-20 15:47:17 +0200

    mails

--- app/admin/controllers/MailsCtrl.js
+++ app/admin/controllers/MailsCtrl.js
@@ -19,7 +19,25 @@ angular.module('app.admin').controller('MailsController', function ($scope, $fil
                 {id: 1, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
                 {id: 2, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
                 {id: 3, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
-                {id: 4, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+                {id: 4, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 5, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 6, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 7, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 8, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 9, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 10, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 11, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 12, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 13, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 14, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 15, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 16, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 17, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 18, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 19, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 20, to_user: 'abc@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 21, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+                {id: 22, to_user: 'xyz@def.com', sent_date:'2017-05-19', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
             ];
         }, 500);
         return;
