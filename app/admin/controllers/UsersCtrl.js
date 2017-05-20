53ee2877d34594b7b777e020b86f57fe9ede9e8b
ericvadim <ericvadim@mail.com>
2017-05-20 15:06:42 +0200

    change password

--- app/admin/controllers/UsersCtrl.js
+++ app/admin/controllers/UsersCtrl.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('app.admin').controller('UsersController', function ($scope) {
+angular.module('app.admin').controller('UsersController', function ($scope, $timeout) {
+    $scope.oldPwd = '';
+    $scope.newPwd = '';
+    $scope.retypePwd = '';
 
-
+    $scope.save = function () {
+        if (confirm('Are you sure want to change password?')) {
+            $scope.loading = true;
+            $timeout(function () {
+                $scope.loading = false;
+                $timeout(function () {
+                    alert('Success!!!');
+                }, 100);
+            }, 500);
+        }
+    };
 });
