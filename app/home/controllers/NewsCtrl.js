b45115c294e18e8563f9429dc9f7e5c151c19641
ericvadim <ericvadim@mail.com>
2017-05-25 04:29:05 +0200

    changed news

--- app/home/controllers/NewsCtrl.js
+++ app/home/controllers/NewsCtrl.js
@@ -5,7 +5,7 @@ angular.module('app.home').controller('HomeNewsController', function ($scope, $w
     $scope.getMonthStrings = function () {
         var now = new Date();
         var y = now.getFullYear();
-        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         for (var m = now.getMonth() + 1; m >= 1; m--) {
             var startDate = moment([y, m - 1]);
             var endDate = moment(startDate).endOf('month');
@@ -31,10 +31,25 @@ angular.module('app.home').controller('HomeNewsController', function ($scope, $w
 
 
     $scope.news = [
-        {id: 1, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", posted_date: "16.05.2017", image: "../styles/img/temp/news/item1.jpg"},
-        {id: 2, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", posted_date: "16.05.2017", image: "../styles/img/temp/news/item2.jpg"},
-        {id: 3, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", posted_date: "16.05.2017", image: "../styles/img/temp/news/item1.jpg"},
-        {id: 4, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ", content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", posted_date: "16.05.2017", image: "../styles/img/temp/news/item2.jpg"}
+        {
+            id: 1,
+            title: "It’s 20 years into the future and the world is a dark, dystopian place, where nation states have merged with multinational corporations to form super-powerful transnational conglomerates.  Locked in a constant war for power and control, the three surviving entities are waging a shadow war using small groups of private paramilitary cyborgs.  These “wolf pack” units specialize in assassination, kidnapping, stealing corporate secrets, and sabotage, often operating with impunity inside the borders of neutral nations.",
+            content: "They rely on an army of augmented humans, whose only purpose is to serve and fight their employers. As an experienced support, assault or recon operative – you must navigate your way in a world of international conspiracy and augmented reality.",
+            posted_date: "16.05.2017",
+            image: "../styles/img/temp/news/item1.jpg"
+        }, {
+            id: 2,
+            title: "Refreshing combination of futuristic cyberpunk game elements and Ghost In the Shell-inspired VR stealth shooter gameplay.",
+            content: "This multiplayer first person shooter is atmospheric and immersive. It’s basic gameplay, but its dark dystopian ambiance will have players exploring in VR space. Firmly set in a cyberpunk high-tech futuristic world, enhanced humans are battling out to protect the interests of international conglomerates that control all resources, trade and territories. Choose whether to fight on the side of United Energy, TekSekProm or XiWu Financial Group with a host of different ways to play (from full-frontal assaults to sneaky backdoor hacking).",
+            posted_date: "16.05.2017",
+            image: "../styles/img/temp/news/item2.jpg"
+        }, {
+            id: 3,
+            title: "Shadowcore is a Ghost in the Shell-inspired, VR-only action title that provides much more than a worthwhile gaming experience. Focusing mainly on the missions and struggle for power of paramilitary organisations directed by international conglomerates, as they battle it out in an cyberpunk world, this action filled game has a lot to offer.",
+            content: "Perfect for those who want high-tech, sleek, and industrial visuals, minimalist aesthetic and a new world in which to immerse themselves. Shdowcore ticks all the boxes when it comes to a stand-alone VR-only FPS with incredible level of immersion, scale and detail!",
+            posted_date: "16.05.2017",
+            image: "../styles/img/temp/news/item1.jpg"
+        }
     ];
 
     $scope.scrollDown = function () {
