c1809094274d1b6880ba9aac16bf9d15c7d7e6be
babybear <edmondd729@gmail.com>
2017-05-29 21:45:45 -0700

    ddd

--- app/app.js
+++ app/app.js
@@ -88,7 +88,7 @@ angular.module('app', [
     })
     .constant('APP_CONFIG', window.appConfig)
 
-    .constant('ServerURL', 'http://serebrumlab.com/api/')
+    .constant('ServerURL', 'http://serebrumlab.com/shadowcore/api/')
     // .constant('ServerURL', 'http://localhost/shadowcore/api/')
     // .constant('ServerURL', 'http://127.0.0.5/')
 
