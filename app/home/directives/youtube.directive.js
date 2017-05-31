01484cb3337b984ec5795017dfe2a445fefc1c1c
ericvadim <ericvadim@mail.com>
2017-05-31 04:33:46 +0200

    home page

--- app/home/directives/youtube.directive.js
+++ app/home/directives/youtube.directive.js
@@ -1,14 +1,25 @@
 angular.module('app').directive('youtube', function ($sce) {
     return {
         restrict: 'EA',
-        template: "<iframe class='video-responsive' src='{{url}}' frameborder='0' allowfullscreen></iframe>",
+        template: '<div id="overlay{{id}}" class="video-overlay" ng-click="play(id);">' +
+        '<img class="" ng-src="{{image}}"/>' +
+        '<div class="play-button"></div>' +
+        '</div>' +
+        '<iframe id="video{{id}}" class="video-responsive" src="{{url}}" frameborder="0" allowfullscreen></iframe>',
         scope: {
+            id: '=id',
+            image: '=image',
             video: '@video'
         },
         link: function (scope) {
-            scope.$watch('video', function (video_id) {
-                scope.url = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + video_id);
+            scope.$watch('video', function (code) {
+                scope.url = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + code + "?showinfo=0");
             });
+            scope.play = function (videoId) {
+                $("#video" + videoId)[0].src += "&autoplay=1";
+                $("#overlay" + videoId).hide();
+                event.preventDefault();
+            };
         }
     };
 });
